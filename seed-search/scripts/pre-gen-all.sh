c18bb4daa58bedf761f28a55d0db716cffd77476
fish4terrisa-MSDSM <flyingfish.msdsm@gmail.com>
2024-07-24 15:46:45 +0000

    updated

--- seed-search/scripts/pre-gen-all.sh
+++ seed-search/scripts/pre-gen-all.sh
@@ -13,8 +13,8 @@ while (( $(echo "${SEED} <= ${END}" |bc -l) ));
 do
 cache="output/seed-${SEED}.jsonl"
 if [ ! -f "$cache" ] ; then
-    cp -f scripts/scrape-seed-7.lua "$CRAWL_PATH"/source/scripts/scrape-seed-7.lua 2>&1 >/dev/null
-    "$GITHUB_WORKSPACE/crawl/source/util/fake_pty" $GITHUB_WORKSPACE/crawl/source/crawl -script scrape-seed-7.lua "$SEED" 8 2>&1 | sed '/^$/d' > "$cache"
+    cp -f scripts/scrape-seed-7.lua "$CRAWL_PATH"/crawl-ref/source/scripts/scrape-seed-7.lua 2>&1 >/dev/null
+    "$GITHUB_WORKSPACE/crawl/crawl-ref/source/util/fake_pty" $GITHUB_WORKSPACE/crawl/crawl-ref/source/crawl -script scrape-seed-7.lua "$SEED" 8 2>&1 | sed '/^$/d' > "$cache"
 fi
 echo "$SEED"
 if (grep -q "MP+" $cache && ( grep -q "Int+6" $cache || grep -q "Int+7" $cache || grep -q "Int+8" $cache || grep -q "Int+9" $cache || grep -q "Int+10" $cache || grep -q "Int+11" $cache || grep -q "Int+12" $cache ) && grep -q "rElec" $cache && (grep -q "Arcjolt" $cache || grep -q "Plasma Beam" $cache)  ) ; then
